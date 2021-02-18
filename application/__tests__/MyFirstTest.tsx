3d1e84c51794a0391189024f2c3e1573f69bad78
barneynicholls <barney.nicholls@bluetoad.net>
2021-02-18 15:16:36 +0000

    adding first test

---
+++ application/__tests__/MyFirstTest.tsx
@@ -0,0 +1,7 @@
+const { isExportDeclaration } = require("typescript");
+
+describe('display of cards', () => {
+    it('displays cards 1-21', () => {
+        expect(true).toBeTruthy();
+    });
+});
